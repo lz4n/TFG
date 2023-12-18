d5a5a2ccd38fdcb8dcc6fd8cf3398808b2f840ba
Izan <70977582+lz4n@users.noreply.github.com>
2023-12-18 13:27:23 +0100

    Add files via upload

--- assets/shaders/hud/fragment.glsl
+++ assets/shaders/hud/fragment.glsl
@@ -7,7 +7,7 @@ Shader de fragmentos del shader HUD.
 in vec2 uvCoords; // Coordenadas de textura de entrada desde el vertex shader
 out vec4 color; // Color de salida
 
-uniform sampler2D uHudTexture; // Textura del HUD
+uniform sampler2D uHudTexture; // Textura del HUD.
 
 void main() {
     color = texture(uHudTexture, uvCoords);
